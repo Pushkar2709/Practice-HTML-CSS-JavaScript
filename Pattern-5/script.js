51a5cf3c17df31a1c27a0871bd3bc196524fab61
Pushkar Maheshwari <pushkar27mah@gmail.com>
2024-05-11 14:48:09 +0530

    updated folder names

--- Pattern-5/script.js
+++
@@ -1,8 +0,0 @@
-const scopeElement = document.getElementById("scope");
-
-document.onpointermove = (event) => {
-    const {clientX, clientY} = event;
-
-    scopeElement.style.left = `${clientX}px`;
-    scopeElement.style.top = `${clientY}px`;
-}
